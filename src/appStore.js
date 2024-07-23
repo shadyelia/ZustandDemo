a88fbe199d67cea4dd8467dd005b2e0b8f872a0d
Shady Elia <shady.elia@hotmail.com>
2024-07-23 17:03:22 +0300

    add presisit

--- src/appStore.js
+++ src/appStore.js
@@ -1,8 +1,17 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { persist, createJSONStorage  } from "zustand/middleware";
 
-export const useCounterStore = create((set) => ({
-  counter: 0,
-  increase: () => set((state) => ({ counter: state.counter + 1 })),
-  reset: () => set({ counter: 0 }),
-  updateCounter: (counter) => set({ counter }),
-}))
+export const useCounterStore = create(
+  persist(
+    (set, get) => ({
+      counter: 0,
+      increase: () => set((state) => ({ counter: get().counter + 1 })),
+      reset: () => set({ counter: 0 }),
+      updateCounter: (counter) => set({ counter }),
+    }),
+    {
+      name: 'counter', // name of the item in the storage (must be unique)
+      //storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  )
+);
